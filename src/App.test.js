5af2de53a13a33ab6106c7f02569e998ca5fdb04
Evan Barton <notrabe1011@gmail.com>
2020-09-03 17:59:26 -0400

    completed mvp

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,16 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("contains input fields", () => {
+  const { getByText } = render(<App />);
+  getByText(/First Name/i);
+  getByText(/Last Name/i);
+  getByText(/Email/i);
+  getByText(/Message/i);
+});
+
+test("has submit button", () => {
+  const { getByTestId } = render(<App />);
+  getByTestId(/submit/i);
+});
